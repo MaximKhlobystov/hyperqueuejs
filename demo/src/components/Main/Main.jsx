bf16392e81c61494b4928aa19ee9d2ea39a9d50f
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2016-02-09 04:38:50 -0500

    Made a consumer resend GET request if connection is closed by producer.

--- demo/src/components/Main/Main.jsx
+++ demo/src/components/Main/Main.jsx
@@ -16,20 +16,24 @@ var Client = (function(brokerHost, brokerPort) {
   var host = brokerHost;
   var port = brokerPort;
 
-  var pullEvent = function(topic, sid, onSuccess, onError) {
+  var consumers = [];
+
+  var pullEvent = function(topic, sid, consumerId, onSuccess, onError) {
+    if(consumers[consumerId] == false) return;
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
       if(xhr.readyState == 4) {
         if(xhr.status == 200) {
           onSuccess(xhr.responseText);
-          pullEvent(topic, sid, onSuccess, onError);
+          pullEvent(topic, sid, consumerId, onSuccess, onError);
         } else {
           if(xhr.status == 204) { // code 204 (no content)
             console.log("No event left");
+            pullEvent(topic, sid, consumerId, onSuccess, onError);
           } else if(xhr.status == 404) { // code 404 (not found)
             console.log("Session not found");
+            onError(xhr.responseText);
           }
-          onError(xhr.responseText);
         }
       }
     }
@@ -52,17 +56,20 @@ var Client = (function(brokerHost, brokerPort) {
       xhr.send(JSON.stringify(event));
     },
     consume: function(topic, onSuccess, onError) {
+      var consumerId = consumers.length;
+      consumers.push(true);
       var req = new XMLHttpRequest();
       req.onreadystatechange = function() {
         if(req.readyState == 4) {
           if(req.status == 200) {
             var sid = req.getResponseHeader("Session-Id");
-            var resp = pullEvent(topic, sid, onSuccess, onError);
+            var resp = pullEvent(topic, sid, consumerId, onSuccess, onError);
           }
         }
       };
       req.open("GET", host + ":" + port + "/" + topic, true);
       req.send();
+      return consumerId;
     }
   };
 });
